81b110a880660a411dcdd257b78706f4435f3624
Wener Castro <wenerwagner@gmail.com>
2026-05-24 12:37:06 -0300

    Add structured API logger

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app.js";
 import { parseEnv } from "./config/env.js";
+import { logger } from "./lib/logger.js";
 
 const env = parseEnv(process.env);
 
 app.listen(env.port, () => {
-  console.log(`API server listening on port ${env.port}`);
+  logger.info({ port: env.port }, "API server listening");
 });
